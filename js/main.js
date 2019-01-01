f52dc7b4394ca72618bf32f8b35c3358a6fea1d5
Benny Ling <sln98106@gmail.com>
2019-01-01 15:54:25 +0800

    update site

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+/* main.js by benny1923 for simple-spa-blog
+   created: 2019-01-01
+   *pray for my final report*
+*/
 var preview;
 window.onload = () => {
      $.ajax({
@@ -6,9 +10,47 @@ window.onload = () => {
          success: function(data){
              preview = data;
              $("#page").empty();
+             $("#news").empty();
              preview.forEach(post => {
                  $("#page").prepend(`<div class="preview"><h1><a href="#${post.link}">${post.title}</a></h1><div>${post.preview}</div></div>`);
+                 $('#news').prepend(`<p><a href="#${post.link}">${post.title} - ${post.data}</a></p>`);
              });
          }
      });
 }
+
+$(window).on('hashchange', function(){
+    try {
+        switch (window.location.hash.substr(1)) {
+            case '':
+            case 'home':
+                $("#page").empty();
+                preview.forEach(post => {
+                    $("#page").prepend(`<div class="preview"><h1><a href="#${post.link}">${post.title}</a></h1><div>${post.preview}</div></div>`);
+                });
+                break;
+            case 'about':
+                $.ajax({
+                    url: 'usercontent/about.html',
+                    dataType: 'html',
+                    success: function (data) {
+                        $("#page").empty();
+                        $("#page").prepend(data);
+                    }
+                })
+                break;
+            default:
+                $.ajax({
+                    url: 'usercontent/post/' + window.location.hash.substr(1) + '.html',
+                    dataType: 'html',
+                    success: function (data) {
+                        $("#page").empty();
+                        $("#page").prepend(data);
+                    }
+                })
+                break;
+        }
+    } catch (error) {
+        
+    }
+})
