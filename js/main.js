06c83afdf04ec3bf65a700cf206a5334e812570e
Benny Ling <sln98106@gmail.com>
2019-01-01 14:44:06 +0800

    update site

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ window.onload = () => {
          dataType: 'json',
          success: function(data){
              preview = data;
+             $("#page").empty();
+             preview.forEach(post => {
+                 $("#page").prepend(`<div class="preview"><h1><a href="#${post.link}">${post.title}</a></h1><div>${post.preview}</div></div>`);
+             });
          }
      });
 }
