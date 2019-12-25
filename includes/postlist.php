5f30f68e6247056d4d73133dd32c72d5d70c05a1
Benny Ling <sln98106@gmail.com>
2019-12-25 14:09:50 +0800

    container css fix and add postlist style stylesheet

--- includes/postlist.php
+++ includes/postlist.php
@@ -1,15 +1,47 @@
 <style>
 .plistf {
+    width: auto;
+    padding: 20px;
+}
+
+.plistf table {
     width: 100%;
+    border-radius: 15px;
+    background-color: #ffffff80;
+    padding: 10px;
+}
+.plistf table th{
+    border-right: black 1px solid;
+    border-radius: 0px;
+}
+.plistf table th:last-child{
+    border-right: none;
+}
+th.title {
+    width: 70%;
+}
+td.date {
+    text-align: center;
+}
+td.manage {
+    text-align: center;
+}
+.plistf table tr td {
+    border-bottom: #cccccc 1px solid;
+    padding-bottom: 3px;
 }
 </style>
 <div class="plistf">
     <table>
         <tr>
-            <td>標題</td>
-            <td>日期</td>
-            <td>管理</td>
+            <th class="title">標題</th>
+            <th class="date">日期</th>
+            <th class="manage">管理</th>
         </tr>
-
+        <tr>
+            <td class="title">WINDOWS 10 完全攻略</td>
+            <td class="date">2019/12/23</td>
+            <td class="manage">刪除&nbsp;&nbsp;&nbsp;編輯</td>
+        </tr>
     </table>
 </div>
